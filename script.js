4df53685895f2ee73b69727476fe0f01045cad5e
Rafal Borynski <rafalborynski@ool-ad020107.dyn.optonline.net>
2018-03-01 14:33:32 -0500

    RafalBorynskiProjectProgress

---
+++ script.js
@@ -0,0 +1,17 @@
+const playerOptions = [
+{choice:"rock", image:"_"},
+{choice:"paper", image:"_"},
+{choice:"scissors", image:"_"},
+]
+
+const computerOptions = [
+{choice:"rock", image:"_"},
+{choice:"paper", image:"_"},
+{choice:"scissors", image:"_"},
+]
+
+console.log(playerOptions[1].choice == computerOptions[1].choice);
+console.log(playerOptions[1].choice == computerOptions[1].choice);
+console.log(playerOptions[2].choice == computerOptions[2].choice);
+
+var userChoice =
